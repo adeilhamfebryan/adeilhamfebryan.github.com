9c8018f656e8ffa56483712f26aae5c08554582b
adeilhamfebryan <51043072+adeilhamfebryan@users.noreply.github.com>
2021-02-28 02:28:44 +0700

    Add files via upload

---
+++ zero_bot.php
@@ -0,0 +1,14 @@
+<?php 
+
+$idbot='bot1617570926:AAFjC62F5g76wB4WjYvSqxDonrFlQFDIEd0';
+$user='997271409';
+
+	function kirim($pesan, $bot, $chat){
+		$url='https://api.telegram.org/'.$bot.'/sendMessage?chat_id='.$chat.'&text='.$pesan.'parse_mode=html';
+		$result=file_get_contents($url,true);
+		return $result;
+	}
+
+kirim(<p>Halo anak anjeng</p>,$idbot,$user);
+
+?>
